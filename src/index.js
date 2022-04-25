01dcdc63f5264da532474202a4067ed2f0508b09
LaviIonas <laviionas772@gmail.com>
2022-04-25 16:21:39 -0400

    issues with client.start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import sdk from 'matrix-js-sdk';
 import { lobi } from './my_login.js';
 
-// const client = sdk.createClient({
-//   baseUrl: "https://matrix.org",
-//   accessToken: "gAxO JocE tZ3U z81a yYA7 dJeV 6I03 pJ3c 6M3X Po1h kbc",
-//   userID: "@lobi-wonas:matrix.org"
+const client = sdk.createClient("https://matrix.org");
+client.login("m.login.password", {"user": lobi.username, "password": lobi.password}).
+  then((response) => {
+    console.log(response.access_token);
+  });
+
+console.log(client.getAccessToken());
+
+
+client.startClient();
+client.once('sync', function(state, prev_state, res) {
+  console.log(state);
+});
+
+// client.on("Room.timeline", function(event, room, to_start_of_timeline) {
+//   console.log(event.event);
+// });
+
+// var rooms = client.getRooms();
+// rooms.forEach(room => {
+//     console.log(room.roomId);
+// });
+
+// var rooms = client.getRooms();
+// rooms.forEach(room => {
+//     var members = room.getJoinedMembers();
+//     members.forEach(member => {
+//         console.log(member.name);
+//     });
 // });
