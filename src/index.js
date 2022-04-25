5de4f322032b46012de6ac81c12f21a965bfe7a1
LaviIonas <laviionas772@gmail.com>
2022-04-25 15:42:41 -0400

    basic .gitignore + test login object

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import sdk from 'matrix-js-sdk';
+import { lobi } from './my_login.js';
+
+// const client = sdk.createClient({
+//   baseUrl: "https://matrix.org",
+//   accessToken: "gAxO JocE tZ3U z81a yYA7 dJeV 6I03 pJ3c 6M3X Po1h kbc",
+//   userID: "@lobi-wonas:matrix.org"
+// });
